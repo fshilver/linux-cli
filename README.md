5a67110073d8908436d86721dbe77a60bbd8ec60
Yang Heesung <fshilver@gmail.com>
2018-05-12 17:07:06 +0900

    add ctail color-command

---
+++ README.md
@@ -0,0 +1,7 @@
+# Custom Linux Command Line Tool
+
+- `showtc` : print terminal color in bash
+- `color.pl` : print terminal color in perl
+- `cpm` : something like rpm command for castis
+- `ctail` : color tail for castis
+- `cp-ott` : copy for castis ott file format
---
+++ color.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use Term::ANSIColor qw/:constants/;
+
+print RED, "Error\n", RESET;
+print YELLOW, "Warning\n", RESET;
+print GREEN, "Warning\n", RESET;
+print BLUE, "Warning\n", RESET;
+print MAGENTA, "Warning\n", RESET;
+print CYAN, "Warning\n", RESET;
